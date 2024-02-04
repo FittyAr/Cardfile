70f815ab882ada850d2e75e4fd54caafb9771eb9
Iván <10284757+iefqar@users.noreply.github.com>
2024-02-04 12:17:34 -0300

    -

--- data/DbContext/SQLAlchemyDbContext.py
+++ data/DbContext/SQLAlchemyDbContext.py
@@ -1,4 +1,4 @@
-from sqlalchemy import BOOLEAN, create_engine, Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy import Boolean, LargeBinary, create_engine, Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from datetime import datetime
@@ -9,7 +9,7 @@ engine = create_engine('sqlite:///database.db', echo=True)
 # Crear una instancia de la clase base declarativa
 Base = declarative_base()
 
-# Definir el modelo de la tabla CardFile
+# Definir el modelo de la tabla Card
 class Card(Base):
     __tablename__ = 'Cards'
     
@@ -18,13 +18,12 @@ class Card(Base):
     Text = Column(String)
     UserId = Column(Integer, ForeignKey('Users.Id'))
     CreatedAt = Column(DateTime, default=datetime.now)
-    Lock = Column(BOOLEAN, default=False)
+    Lock = Column(Boolean, default=False)
 
     User = relationship('User', back_populates='CardFiles')
-    attached_files = relationship('AttachedFile', back_populates='CardFile')
+    attached_files = relationship('AttachedFile', back_populates='card')
     
     def __str__(self):
-        #return f'Card(Id: {self.Id}, Name: {self.Name}, Text: {self.Text}, UserId: {self.UserId}, CreatedAt: {self.CreatedAt}, Lock: {self.Lock})'
         return f'{self.Name}'
 
 # Definir el modelo de la tabla User
@@ -40,11 +39,10 @@ class User(Base):
     Normalized_Email = Column(String, nullable=False, unique=True)
     CreatedAt = Column(DateTime, default=datetime.now)
 
-    CardFiles = relationship('CardFile', back_populates='User')
+    card_files = relationship('Card', back_populates='User')
     
     def __str__(self):
         return f'{self.Name}'
-
 
 # Definir el modelo de la tabla AttachedFile
 class AttachedFile(Base):
@@ -52,13 +50,13 @@ class AttachedFile(Base):
 
     Id = Column(Integer, primary_key=True)
     FileName = Column(String)
-    File = Column(object)
-    CardFileId = Column(Integer, ForeignKey('CardFiles.Id'))
+    File = Column(LargeBinary)
+    CardId = Column(Integer, ForeignKey('Cards.Id'))
 
-    CardFile = relationship('CardFile', back_populates='AttachedFiles')
+    card = relationship('Card', back_populates='attached_files')
     
     def __str__(self):
         return f'{self.FileName}'
 
 # Crear las tablas en la base de datos
-Base.metadata.create_all(engine) 
+Base.metadata.create_all(engine)
